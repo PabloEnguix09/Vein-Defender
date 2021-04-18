83d68dbdc70b41ebdcd29224680d2da1f54f5dd4
Luis Belloch <luis.bellochmartinez@gmail.com>
2021-04-18 13:26:06 +0200

    Enemigo objeto
    Creado SO de Enemigo Básico, cambios en la animación de Enemigo Bomba

--- Assets/Scripts/Bomba.cs
+++ Assets/Scripts/Bomba.cs
@@ -21,17 +21,9 @@ public class Bomba : MonoBehaviour
     Animator animator;
     public float destroyOfftime = 0.5f;
 
-    [Range(0, 1)]
-    public float fuerza = .3f;
-
-    [Range(0, 1)]
-    public float vida = .5f;
-
-    public float velocidad = 10;
-
-    public float rango = 4;
-
     public float radioExplosion = 2f;
+
+    public EnemigoBasico enemigoBasico;
 
     private Enemigo enemigo;
 
@@ -39,16 +31,11 @@ public class Bomba : MonoBehaviour
     void Start()
     {
         enemigo = gameObject.GetComponent<Enemigo>();
-        enemigo.fuerza = fuerza;
-        enemigo.vida = vida;
-        enemigo.vision = rango;
-        enemigo.agente.speed = velocidad;
-        animator = gameObject.GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (enemigo.vida <= 0)
+        if (enemigo.vidaActual <= 0)
         {
             // Inicia la animacion de explotar
             animator.SetBool("Explode", true);
@@ -68,28 +55,28 @@ public class Bomba : MonoBehaviour
                     if (colliders[i].CompareTag("Bases"))
                     {
                         Base estructura = colliders[i].gameObject.GetComponent<Base>();
-                        estructura.Salud -= enemigo.fuerza;
+                        estructura.Salud -= enemigoBasico.ataque;
                         //Debug.Log("Golpeo una base");
                     }
 
                     else if (colliders[i].CompareTag("Torretas"))
                     {
                         Torreta estructura = colliders[i].gameObject.GetComponent<Torreta>();
-                        estructura.vidaActual -= enemigo.fuerza;
+                        estructura.vidaActual -= enemigoBasico.ataque;
                         //Debug.Log("Golpeo una torrerta");
                     }
 
                     else if (colliders[i].CompareTag("Enemigos"))
                     {
                         Enemigo otroEnemigo = colliders[i].gameObject.GetComponent<Enemigo>();
-                        otroEnemigo.Vida -= enemigo.fuerza;
+                        otroEnemigo.vidaActual -= enemigoBasico.ataque;
                         //Debug.Log("Golpeo un enemigo");
                     }
 
                     else if (colliders[i].CompareTag("Player"))
                     {
                         Personaje personaje = colliders[i].gameObject.GetComponent<Personaje>();
-                        personaje.Salud -= enemigo.fuerza;
+                        personaje.Salud -= enemigoBasico.ataque;
                         //Debug.Log("Golpeo al jugador");
                     }
                 }
@@ -107,7 +94,7 @@ public class Bomba : MonoBehaviour
         if (other.gameObject.GetComponent<Base>() != null || other.gameObject.GetComponent<Torreta>() != null || other.gameObject.GetComponent<Personaje>())
         {
             //Debug.Log("Choque");
-            enemigo.vida = 0;
+            enemigo.vidaActual = 0;
         }
     }
 
---
+++ Assets/Scripts/EnemigoBasico.cs
@@ -0,0 +1,23 @@
+// ---------------------------------------------------
+// NAME: EnemigoBasico
+// STATUS: WIP
+// GAMEOBJECT: sistema
+// DESCRIPTION: maneja la creacion de los objetos Enemigo y sus datos
+//
+// AUTHOR: Luis
+// FEATURES ADDED: Creado el Scriptable Object de las torretas
+// ---------------------------------------------------
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemigo", menuName = "Objetos/Enemigo", order = 1)]
+public class EnemigoBasico : ScriptableObject
+{
+    public float ataque;
+    public float vidaMaxima;
+    public float velocidad;
+    public float rango;
+    public enum tipo
+    {
+        terrestre, volador, subterraneo
+    }
+}
