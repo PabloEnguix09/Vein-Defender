b32d562f01d08ace49d0eb32df315b38cde6029a
Juan <tostyfis360@gmail.com>
2021-05-26 13:14:19 +0200

    Scrip de animaciones
    No esta terminado

---
+++ Assets/Scripts/AnimTByte.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ---------------------------------------------------
+// NAME: AnimTByte.cs
+// STATUS: WIP
+// GAMEOBJECT: Jugador
+// DESCRIPTION: Controla las animaciones del jugador
+//
+// AUTHOR: Juan Ferrera Sala
+// FEATURES ADDED: 
+// ---------------------------------------------------
+public class AnimTByte : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SeleccionDeTorreta()
+    {
+        animator.SetTrigger("SeleccionDeTorreta");
+    }
+
+    public void InvocarTorreta()
+    {
+        animator.SetTrigger("InvocarTorreta");
+    }
+
+    public void LanzarDardo()
+    {
+        animator.SetTrigger("Dardo");
+    }
+
+    public void Salto()
+    {
+        animator.SetTrigger("Salto");
+    }
+
+    public void Muerte()
+    {
+        animator.SetBool("Muerte", true);
+    }
+}
