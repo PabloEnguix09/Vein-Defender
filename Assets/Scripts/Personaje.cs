865935fef4b4e50a31dd660dab1e212b836d6bea
Adrian <admallla@epsg.upv.es>
2021-06-15 15:44:39 +0200

    Cinemática to guapa

--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -204,7 +204,7 @@ public class Personaje : MonoBehaviour
         // Cierra el menu actual
         if(Input.GetButtonDown("Cancelar"))
         {
-            gui.SetActive(true);
+            
             CerrarInteraccion();
         }
         
